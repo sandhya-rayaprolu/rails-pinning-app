807c1c160e5037a0a75de5e28f6af4a01713c0cf
Sandhya Rayaprolu <sandhya.rayaprolu@gmail.com>
2015-08-21 23:06:53 +0000

    Add named route for show-by-name route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pins#index'
 
+  get '/pins/name-:slug' => 'pins#show_by_name', as: 'pin_by-name'
+
   resources :pins
   
   get '/library' => 'pins#index'
+  
     
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
